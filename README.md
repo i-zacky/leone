c74c0c183e84fbc67c50308bac8abe00f568bc13
i-zacky <aizaki.takayuki@gmail.com>
2021-12-29 01:03:41 +0900

    add kms / cognito CFn

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# leone
+# Leone Meccanica
+
+Project Template by AWS CloudFormation
+
+## Overview
+
+### aws/env
+
+Environment variables for CloudFormation
+
+### aws/scripts
+
+Create or Update CloudFormation stack
+
+### aws/templates
+
+CloudFormation templates
+
+## Requirements
+
+### System / Tools
+
+- direnv
+
+## Setup
+
+### Activate direnv
+
+```sh
+$ cp .envrc.origin .envrc
+$ direnv allow
+```
---
+++ scripts/kms.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-0}")" && pwd)"
+REPOSITORY_ROOT_DIR="$(dirname "${SCRIPT_DIR}")"
+
+source "${REPOSITORY_ROOT_DIR}/config/dev"
+
+TEMPLATE_FILE=${KMS_TEMPLATE}
+STACK_NAME=${KMS_STACK_NAME}
+
+EXIST_CHECK=$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query 'Stacks[].StackName | [0]' --output text)
+if [ "${EXIST_CHECK}" = "${STACK_NAME}" ]; then
+  METHOD=update-stack
+else
+  METHOD=create-stack
+fi
+
+# create or update stack
+echo "${METHOD} ${STACK_NAME}"
+aws cloudformation ${METHOD} \
+    --stack-name "${STACK_NAME}" \
+    --template-body "file://${REPOSITORY_ROOT_DIR}/templates/${TEMPLATE_FILE}" \
+    --region "${REGION}" \
+    --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
+    --parameters \
+      ParameterKey=Project,ParameterValue=${PROJ} \
+      ParameterKey=Env,ParameterValue=${ENV} \
+    --tags \
+      Key=Project,Value=${PROJ} \
+      Key=Environment,Value=${ENV}
+
+# wait create or update stack complete
+SUCCESS=('CREATE_COMPLETE' 'UPDATE_COMPLETE')
+FAILED=('ROLLBACK_COMPLETE' 'UPDATE_ROLLBACK_COMPLETE' 'ROLLBACK_FAILED' 'UPDATE_ROLLBACK_FAILED' 'ROLLBACK_IN_PROGRESS' 'UPDATE_ROLLBACK_IN_PROGRESS')
+for i in {1..90}
+do
+  STATE=$(aws cloudformation describe-stacks --stack-name ${STACK_NAME} --region ${REGION} --query 'Stacks[].StackStatus' --output text)
+  if printf '%s\n' "${SUCCESS[@]}" | grep -qx "${STATE}" > /dev/null >&2; then
+    break
+  fi
+  if printf '%s\n' "${FAILED[@]}" | grep -qx "${STATE}" > /dev/null >&2; then
+    echo "Rollback stack ${STACK_NAME}"
+    exit 1
+  fi
+  echo "wait 20 sec..."
+  sleep 20
+done
+
+echo "Complete ${METHOD} ${STACK_NAME}"
