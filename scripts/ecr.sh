bd22da84cfd4ac7ac8394b0e2bbd70fb108baf87
i-zacky <aizaki.takayuki@gmail.com>
2022-01-14 16:09:11 +0900

    add ecr cfn

---
+++ scripts/ecr.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-0}")" && pwd)"
+REPOSITORY_ROOT_DIR="$(dirname "${SCRIPT_DIR}")"
+
+source "${REPOSITORY_ROOT_DIR}/env/$1"
+
+TEMPLATE_FILE=${ECR_TEMPLATE}
+STACK_NAME=${ECR_STACK_NAME}
+
+sam deploy \
+  --region "${REGION}" \
+  --template "${REPOSITORY_ROOT_DIR}/templates/${TEMPLATE_FILE}" \
+  --stack-name "${STACK_NAME}" \
+  --s3-bucket "${CF_TEMPLATE_BUCKET}" \
+  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+  --no-fail-on-empty-changeset \
+  --tags \
+      Env="${ENV}" \
+      Project="${PROJ}" \
+  --parameter-overrides \
+      Project=${PROJ} \
+      Env=${ENV}
